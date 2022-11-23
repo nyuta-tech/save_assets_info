38b6b302fb1d0e3221276917ef12aa128760f744
nyuta <nyuta.main@gmail.com>
2022-11-23 08:09:32 +0000

    ✨ feat crypto module

---
+++ src/utils/crypto.rs
@@ -0,0 +1,15 @@
+use sha2::Sha256;
+use hmac::{Hmac, Mac};
+
+pub fn sign_hmac(api_secret:&str, msg:&str) -> String{
+    // Create alias for HMAC-SHA256
+    type HmacSha256 = Hmac<Sha256>;
+    let mut mac = HmacSha256::new_from_slice(api_secret.as_bytes()).expect("Success");
+    mac.update(msg.as_bytes());
+
+    let result = mac.finalize();
+    let code_bytes = result.into_bytes();
+    let signature = format!("{:X}", code_bytes);
+
+    signature.to_lowercase()
+}
